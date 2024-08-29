f0efefbceb375e7ef4ec03d0f10e0a6f9610254f
Siparat <akimov1712@yandex.ru>
2024-08-29 15:52:21 +0300

    feat(app): Added message module

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -1,7 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+	BadRequestException,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	HttpStatus,
+	Param,
+	ParseIntPipe,
+	Post,
+	UseGuards
+} from '@nestjs/common';
 import { MessageService } from './message.service';
+import { MessageModel, UserModel } from '@prisma/client';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { User } from 'src/decorators/user.decorator';
+import { DatabaseService } from 'src/database/database.service';
+import { MessageErrorMessages } from './message.constants';
 
 @Controller('message')
 export class MessageController {
-	constructor(private readonly messageService: MessageService) {}
+	constructor(
+		private readonly messageService: MessageService,
+		private database: DatabaseService
+	) {}
+
+	@UseGuards(JwtAuthGuard)
+	@Get('all')
+	getAll(@User() user: UserModel): Promise<MessageModel[]> {
+		return this.database.messageModel.findMany({ where: { user: { id: user.id } } });
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@HttpCode(HttpStatus.OK)
+	@Post('preview/:id')
+	async previewMessage(@Param('id', ParseIntPipe) id: number, @User() user: UserModel): Promise<void> {
+		if (!user.tgId) {
+			throw new BadRequestException(MessageErrorMessages.TG_ID_IS_NULL);
+		}
+		await this.messageService.previewMessage(id, user.tgId);
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Delete(':id')
+	deleteById(@Param('id', ParseIntPipe) id: number, @User() user: UserModel): Promise<MessageModel> {
+		return this.messageService.deleteMessage(id, user.tgId);
+	}
 }
--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -1,19 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { MessageModel } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
 import { MessageEntity } from './entities/message.entity';
+import { MessageErrorMessages } from './message.constants';
+import { BotService } from 'src/bot/bot.service';
 
 @Injectable()
 export class MessageService {
-	constructor(private database: DatabaseService) {}
+	constructor(
+		private database: DatabaseService,
+		private bot: BotService
+	) {}
 
-	createMessage(entity: MessageEntity): Promise<MessageModel> {
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		return this.database.messageModel.create({ data: entity as any });
+	async deleteMessage(id: number, userTgId: number | null): Promise<MessageModel> {
+		const message = await this.database.messageModel.findUnique({ where: { id } });
+		if (!message) {
+			throw new NotFoundException(MessageErrorMessages.NOT_FOUND);
+		}
+		if (message.userTgId !== userTgId) {
+			throw new ForbiddenException(MessageErrorMessages.FORBIDDEN);
+		}
+		return this.database.messageModel.delete({ where: { id } });
 	}
 
-	updateMessage(id: number, entity: MessageEntity): Promise<MessageModel> {
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		return this.database.messageModel.update({ where: { id }, data: entity as any });
+	async previewMessage(id: number, userTgId: number): Promise<void> {
+		const message = await this.database.messageModel.findUnique({ where: { id } });
+		if (!message) {
+			throw new NotFoundException(MessageErrorMessages.NOT_FOUND);
+		}
+		if (message.userTgId !== userTgId) {
+			throw new ForbiddenException(MessageErrorMessages.FORBIDDEN);
+		}
+		const entity = new MessageEntity(message);
+		await this.bot.sendMessage(userTgId, entity);
 	}
 }
